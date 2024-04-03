bd791ae1eca48067a77b850f7ebe12ae9f93aab2
Vera Nicolas <90095696+Arquake@users.noreply.github.com>
2024-04-03 15:31:38 +0200

    p

--- lib/nodemaxocc.ml
+++ lib/nodemaxocc.ml
@@ -9,10 +9,10 @@ let create : data -> answer =
 let combine : node -> node -> node =
  fun n1 n2 ->
   match n1.answer with
-  | (e1,e2) when fst n2.answer > e1 -> {answer = (fst n2.answer,snd n2.answer); left = n1.left; right = n2.right }
+  | (e1,_) when fst n2.answer > e1 -> {answer = (fst n2.answer,snd n2.answer); left = n1.left; right = n2.right }
   | (e1,e2) when fst n2.answer < e1 -> {answer = (e1,e2); left = n1.left; right = n2.right }
-  | (e1,e2) when fst n2.answer = e1 -> {answer = (0,0); left = n1.left; right = n2.right }
+  | (e1,_) when fst n2.answer = e1 -> {answer = (0,0); left = n1.left; right = n2.right }
 
 let to_string : answer -> string = 
  function
- | (first,second) -> "(" ^ (string_of_int first) ^ "." ^ (string_of_int second) ^ ")" 
+ | (first,second) -> "(" ^ (string_of_int first) ^ ", " ^ (string_of_int second) ^ ")" 
--- lib/query.ml
+++ lib/query.ml
@@ -11,25 +11,29 @@ module Make (N : NODE) : QUERY_STRUCTURE = struct
   let create : data list -> tree = 
    fun list->
     let leave = fun l value ->
-      let l1 = l.left = value in let l2 = l1.right = value
+      Leaf({node = {answer = N.create l; left = value; right = value}})
     in
     let rec create_leaves = fun list rest ->
       match list with
-      | h::[] -> rest @ [leave Leaf(h) List.length rest]
+      | h::[] -> rest @ [leave h (List.length rest)]
       | h::t -> create_leaves t (rest @ [Leaf(h)])
     in
     let rec create_recursively = fun elements rest ->
       match elements with
       | [] -> create_recursively rest []
       | h::[] -> h
-      | h1::h2::t when h1.node > h2.node -> rest @ [{node = N.combine h1, h2; right_child = h1 ; left_child = h2}]
+      | h1::h2::t -> rest @ [{node = N.combine h1, h2; right_child = h2 ; left_child = h1}]
     in
     (create_recursively (create_leaves list []) [])
 
-  (* Mise à jour d’un élément de la liste *)
-  let update : tree -> data -> int -> tree =
-   fun arbre to_update the_update  ->
-
+  (* Mise à jour d’un élément de la liste data nouvelle valeur et int l'indice dans le tableau*)
+  let update : tree -> data -> int -> tree = fun arbre number position  ->
+    let rec research_and_replace =
+      function
+      | Leaf(x) -> Leaf({node = {answer = N.create number; left = position; right = position}})
+      | Node(x) when x.left + ((x.right-x.left) / 2) >= int -> Node({node = N.combine; left_child = (research_and_replace x.left); right_child = (x.right)})
+      | Node(x) when x.left + ((x.right-x.left) / 2) <= int -> Node({node = N.combine; left_child = (x.left); right_child = (research_and_replace x.right)})
+    in research_and_replace arbre
    
 
   let query : tree -> int -> int -> answer =
