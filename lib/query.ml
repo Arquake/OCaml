0331c4514afbf495b4764d4011571b7db49ba32a
Aminmiri82 <miri.amin@ymail.com>
2024-04-12 16:12:21 +0200

    stuff

--- lib/query.ml
+++ lib/query.ml
@@ -8,57 +8,59 @@ module Make (N : NODE) : QUERY_STRUCTURE = struct
   let to_string = N.to_string
 
   (* Création de l’arbre *)
-  let create : data list -> tree = 
-   fun list->
+  let create : data list -> tree = fun list ->
     let leave = fun l value ->
-      Leaf({node = {answer = N.create l; left = value; right = value}})
+      Leaf ({ node = { answer = N.create l; left = value; right = value } })
     in
-    let rec create_leaves = fun list rest ->
+    let rec create_leaves list rest =
       match list with
-      | h::[] -> rest @ [leave h (List.length rest)]
-      | h::t -> create_leaves t (rest @ [Leaf(h)])
+      | h :: [] -> rest @ [leave h (List.length rest)]
+      | h :: t -> create_leaves t (rest @ [Leaf { node = h }])
     in
-    let rec create_recursively = fun elements rest ->
+    let rec create_recursively elements rest =
       match elements with
       | [] -> create_recursively rest []
-      | h::[] -> h
-      | h1::h2::t -> rest @ [{node = N.combine h1, h2 ; left_child = h1; right_child = h2}]
-    (create_recursively (create_leaves list []) [])
+      | h :: [] -> h
+      | h1 :: h2 :: t -> rest @ [Node { node = N.combine h1.node h2.node; left_child = h1; right_child = h2 }]
+    in
+    create_recursively (create_leaves list []) []
 
-  (* Mise à jour d’un élément de la liste data nouvelle valeur et int l'indice dans le tableau*)
-  let update : tree -> data -> int -> tree = fun arbre number position  ->
-    let rec research_and_replace =
-      function
-      | Leaf(x) -> 
-        if x.left = position && x.right = position then
-          Leaf({node = {answer = N.create number; left = position; right = position}})
+  (* Mise à jour d’un élément de la liste *)
+  let update : tree -> data -> int -> tree = fun arbre number position ->
+    let rec research_and_replace = function
+      | Leaf { node } ->
+        if node.left = position && node.right = position then
+          Leaf { node = { answer = N.create number; left = position; right = position } }
         else
-          Leaf(x)
-      (*just to make sure that if something goes wrong with the traversal the whole leafs dont get fucked up*)    
-      | Node(x) when x.left + ((x.right-x.left) / 2) >= int -> Node({node = N.combine; left_child = (research_and_replace x.left); right_child = (x.right)})
-      | Node(x) when x.left + ((x.right-x.left) / 2) <= int -> Node({node = N.combine; left_child = (x.left); right_child = (research_and_replace x.right)})
-    in research_and_replace arbre
+          Leaf { node }
+      | Node { node; left_child; right_child } ->
+        if position <= node.left + ((node.right - node.left) / 2) then
+          let updated_left_child = research_and_replace left_child in
+          Node { node = N.combine updated_left_child.node right_child.node; left_child = updated_left_child; right_child }
+        else
+          let updated_right_child = research_and_replace right_child in
+          Node { node = N.combine left_child.node updated_right_child.node; left_child; right_child = updated_right_child }
+    in
+    research_and_replace arbre
 
-    let query : tree -> int -> int -> answer = fun tree l r ->
-      let rec aux node l r =
-        match node with
-        | Leaf { node = n } ->
-          if n.left >= l && n.right <= r then
-            n.answer
-          else
-            failwith "Outside the range"  
-        | Node { node = n; left_child = lc; right_child = rc } ->
-          if r < n.left || l > n.right then
-            failwith "Outside the range" 
-          else if l <= n.left && r >= n.right then
-            n.answer  (* this only happens when the query gods smile upon us*)
-            
-          else (*this happens when the query gods aren't satisfied with our blood sacrifises *)
-           let left_node = if l <= n.left + ((n.right - n.left) / 2) then aux lc l r else { answer = N.create 0; left = n.left; right = n.left + ((n.right - n.left) / 2) } in
-           let right_node = if r > n.left + ((n.right - n.left) / 2) then aux rc l r else { answer = N.create 0; left = n.left + ((n.right - n.left) / 2) + 1; right = n.right } in
-            (*i'm pretty sure these are right*)
-           (N.combine left_node right_node).answer
-      in
-      aux tree l r
+  let query : tree -> int -> int -> answer = fun tree l r ->
+    let rec aux node l r =
+      match node with
+      | Leaf { node = n } ->
+        if n.left >= l && n.right <= r then
+          n.answer
+        else
+          failwith "Outside the range"
+      | Node { node = n; left_child = lc; right_child = rc } ->
+        if r < n.left || l > n.right then
+          failwith "Outside the range"
+        else if l <= n.left && r >= n.right then
+          n.answer
+        else
+          let left_node = if l <= n.left + ((n.right - n.left) / 2) then aux lc l r else { node = { answer = N.create 0; left = n.left; right = n.left + ((n.right - n.left) / 2) } } in
+          let right_node = if r > n.left + ((n.right - n.left) / 2) then aux rc l r else { node = { answer = N.create 0; left = n.left + ((n.right - n.left) / 2) + 1; right = n.right } } in
+          N.combine left_node right_node
+    in
+    aux tree l r
     
 end
