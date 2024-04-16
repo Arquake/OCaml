9c7a9fdaf5d9a42e3fbaf6fb442db0bf67ca8521
Vera Nicolas <90095696+Arquake@users.noreply.github.com>
2024-04-16 15:56:50 +0200

    Update nodemaxocc.ml

--- lib/nodemaxocc.ml
+++ lib/nodemaxocc.ml
@@ -10,8 +10,8 @@ let combine : node -> node -> node =
  fun n1 n2 ->
   match n1.answer with
   | (e1,_) when fst n2.answer > e1 -> {answer = (fst n2.answer,snd n2.answer); left = n1.left; right = n2.right }
-  | (e1,_) when fst n2.answer = e1 -> {answer = (0,0); left = n1.left; right = n2.right }
-  | (e1,e2) -> {answer = (e1,e2); left = n1.left; right = n2.right }
+  | (e1,occ) when fst n2.answer = e1 -> {answer = (e1,snd n2.answer + occ); left = n1.left; right = n2.right }
+  | ele -> {answer = ele; left = n1.left; right = n2.right }
 
 let to_string : answer -> string = 
  function
