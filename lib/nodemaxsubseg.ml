1394b4b8660a06d50f747d7c3b544818f46ff1e6
Vera Nicolas <90095696+Arquake@users.noreply.github.com>
2024-04-18 15:01:40 +0200

    push
    j'ai push sur un projet perso et j'ai écrit dedans que j'allais me harakiri

--- lib/nodemaxsubseg.ml
+++ lib/nodemaxsubseg.ml
@@ -3,21 +3,24 @@ type answer = { sum : int; prefix : int; suffix : int; subseg : int }
 type node = { answer : answer; left : int; right : int }
 
 let create : data -> answer = 
- fun element -> { sum = element; prefix = 0; suffix = 0; subseg = 0 }
+ fun element -> { sum = element; prefix = element; suffix = element; subseg = element }
 
 let combine : node -> node -> node =
-fun left_node right_node ->
+  fun left_node right_node ->
 
-  let max = if left_node.answer.sum > right_node.answer.sum then left_node.answer.sum else right_node.answer.sum in
-  let ans = { sum = (left_node.answer.sum + right_node.answer.sum ); prefix = left_node.answer.prefix ; suffix = right_node.answer.prefix ; subseg = max }; 
-  in
-  {
-    answer = ans;
-    left = left_node.left ;
-    right = right_node.right
-  }
+    let max = fun x y -> if x > y then x else y in
+    let ans = { sum = (left_node.answer.sum + right_node.answer.sum );
+                prefix = max (left_node.answer.prefix) (left_node.answer.sum + right_node.answer.prefix) ;
+                suffix = max (right_node.answer.prefix) (right_node.answer.sum + left_node.answer.prefix) ;
+                subseg = max (max (left_node.answer.subseg) (right_node.answer.subseg)) (left_node.answer.suffix + right_node.answer.prefix) };
+    in
+    {
+      answer = ans;
+      left = left_node.left ;
+      right = right_node.right
+    }
 
 
 let to_string : answer -> string = 
- fun element ->
-  string_of_int (element.sum)
+  fun element ->
+    string_of_int (element.subseg)
